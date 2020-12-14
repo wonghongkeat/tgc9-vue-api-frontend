197bb6224efb41a06b1a60ff07f044fc07cb1d51
wonghongkeat <hongkeat1991@gmail.com>
2020-12-14 05:26:10 +0000

    edited

---
+++ api-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <Faults/>
+  </div>
+</template>
+
+<script>
+import Faults from './components/Faults';
+
+
+export default {
+  name: 'App',
+  components: {
+      Faults
+  }
+}
+</script>
+
+<style>
+
+</style>
